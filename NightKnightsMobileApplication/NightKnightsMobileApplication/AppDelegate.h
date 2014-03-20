699a491daa67069177125129a2340092979eff4f
Ellyonia <ckeith@smu.edu>
2014-03-20 18:46:09 -0500

    Created Base application with no customizations yet.

---
+++ NightKnightsMobileApplication/NightKnightsMobileApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NightKnightsMobileApplication
+//
+//  Created by Cameron Keith on 3/20/14.
+//  Copyright (c) 2014 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NightKnightsMobileApplication/NightKnightsMobileApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NightKnightsMobileApplication
+//
+//  Created by Cameron Keith on 3/20/14.
+//  Copyright (c) 2014 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
