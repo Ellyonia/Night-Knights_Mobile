d5aac57ce461ad332da295bd9f375219e5740666
Ellyonia <ckeith@smu.edu>
2014-03-20 19:02:31 -0500

    Added Title text to Login page, Added button for Login and to go to Account Creation, Added labels and Text fields for User Information for Login.

---
+++ NightKnightsMobileApplication/NightKnightsMobileApplication/ViewControllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  NightKnightsMobileApplication
+//
+//  Created by Cameron Keith on 3/20/14.
+//  Copyright (c) 2014 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
