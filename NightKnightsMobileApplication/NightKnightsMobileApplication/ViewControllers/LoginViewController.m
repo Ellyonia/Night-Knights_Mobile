4f12d5345e69ffac021faa53a2511f480cbb3660
Ellyonia <ckeith@smu.edu>
2014-03-20 19:08:10 -0500

    Added outlets for the TextFields to LoginViewController.m as well as an Action function to the Login Button.

--- NightKnightsMobileApplication/NightKnightsMobileApplication/ViewControllers/LoginViewController.m
+++ NightKnightsMobileApplication/NightKnightsMobileApplication/ViewControllers/LoginViewController.m
@@ -9,10 +9,15 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *emailTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
 
 @implementation LoginViewController
+- (IBAction)login:(UIButton *)sender {
+    
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -34,16 +39,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
