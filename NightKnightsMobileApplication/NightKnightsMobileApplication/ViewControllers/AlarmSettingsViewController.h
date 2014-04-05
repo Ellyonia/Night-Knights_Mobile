8cb18ce8ade4b35e4af8819dc8740a291107d65d
Ellyonia <ckeith@smu.edu>
2014-04-05 12:32:02 -0500

    Create the AlarmSettingViewController and added basic logic on how to navigate to and from the view. Also edited the CreatAccountView’s login button to say Cancel and moved its location to the top left corner of the View.

---
+++ NightKnightsMobileApplication/NightKnightsMobileApplication/ViewControllers/AlarmSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlarmSettingsViewController.h
+//  NightKnightsMobileApplication
+//
+//  Created by Cameron Keith on 4/5/14.
+//  Copyright (c) 2014 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlarmSettingsViewController : UIViewController
+
+@end
---
+++ NightKnightsMobileApplication/NightKnightsMobileApplication/ViewControllers/AlarmSettingsViewController.m
@@ -0,0 +1,35 @@
+//
+//  AlarmSettingsViewController.m
+//  NightKnightsMobileApplication
+//
+//  Created by Cameron Keith on 4/5/14.
+//  Copyright (c) 2014 DreamTeam. All rights reserved.
+//
+
+#import "AlarmSettingsViewController.h"
+
+@interface AlarmSettingsViewController ()
+
+@end
+
+@implementation AlarmSettingsViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
